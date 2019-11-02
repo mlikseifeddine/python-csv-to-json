1273a31a9c20b40852b6dac6cc9274d619962da9
Mlik Saifeddine <mliksaifeddine@outlook.com>
2019-11-02 16:52:29 +0100

    trying to fix language decode

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-import csv, json
-open( 'output.json', 'w').write(json.dumps([row for row in csv.DictReader(open( 'listings.csv', 'r' ))], indent=4))
+import csv, json, codecs
+
+codecs.open('output.json', 'w').write(json.dumps([row for row in csv.DictReader(codecs.open(r'listings.csv', encoding='latin1'))], indent=4))
